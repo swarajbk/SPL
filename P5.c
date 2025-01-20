d83bd200404e466a8a0e90fc18af7449d66a05e1
Swaraj Bakali <swaraj.bakali22@gmail.com>
2025-01-20 22:58:41 +0600

    C PROGRAME

---
+++ P5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i[6],a[i]=n;
+    n=2001,2000,2002,2003,2004,2005;
+    for(a[i]=0;a[i]<6;a[i]++)
+
+    if((a[i]%400==0)&&(a[i]%100!=0)||(a[i]%4==0))
+      printf("%d is leap year number\n",a[i]);
+        else
+        printf("%d is not leap year number\n",a[i]);
+}
